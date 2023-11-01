5832d74fea30ae80eec8cb8c6e6ffc42b786249d
Eric Niyonkuru <eric.niyonkuru@pesachoice.com>
2023-11-02 00:35:39 +0200

    add reusable component for footer

--- src/components/layouts/PublicLayout.tsx
+++ src/components/layouts/PublicLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./navigation/NavBar";
+import Footer from "./Footer";
 const PublicLayout = () => {
   return (
     <>
       <Navbar />
       <Outlet />
+      <Footer />
     </>
   );
 };
