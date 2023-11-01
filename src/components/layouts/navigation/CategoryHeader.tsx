7e813cf621f6167a438c4a9d57b8bf22abb3935c
muhireheir <muhire416@gmail.com>
2023-11-01 18:50:52 +0200

    slider

--- src/components/layouts/navigation/CategoryHeader.tsx
+++ src/components/layouts/navigation/CategoryHeader.tsx
@@ -1,13 +1,13 @@
 const CategoryHeader = () => {
-    return (
-        <section className="w-full bg-action-color-500 text-white p-3 flex gap-4 overflow-auto category-strip">
-            <div className="shrink-0">Category name</div>
-            <div className="shrink-0">Category name</div>
-            <div className="shrink-0 active">Category name</div>
-            <div className="shrink-0">Category name</div>
-            <div className="shrink-0">Category name</div>
-        </section>
-    )
-}
+  return (
+    <section className='w-full px-6 md:px-12 lg:px-24 bg-action-color-500 text-white p-3 flex gap-4 overflow-auto category-strip'>
+      <div className='shrink-0'>Category name</div>
+      <div className='shrink-0'>Category name</div>
+      <div className='shrink-0 active'>Category name</div>
+      <div className='shrink-0'>Category name</div>
+      <div className='shrink-0'>Category name</div>
+    </section>
+  );
+};
 
-export default CategoryHeader
+export default CategoryHeader;
---
+++ src/components/slider/Slider.tsx
@@ -0,0 +1,38 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import image1 from "../../assets/istockphoto-1445195721-1024x1024.jpg";
+import image2 from "../../assets/pexels-karolina-grabowska-5632398.jpg";
+const Slider = () => {
+  return (
+    <Swiper
+      slidesPerView={1}
+      autoplay={{
+        delay: 5000,
+      }}
+      pagination={{ clickable: true }}
+      modules={[Pagination, Autoplay]}
+      loop={true}
+    >
+      <SwiperSlide>
+        <div className='w-full h-52 md:h-96  lg:h-[500px] bg-red-600'>
+          <img
+            src={
+              "https://res.cloudinary.com/umuhire-heritier/image/upload/w_1666,h_400,c_fill/Home-image_1_pvyrks.png"
+            }
+            alt=''
+            className='w-full h-full object-cover'
+          />
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className='w-full h-52 md:h-96  lg:h-[500px] bg-green-600'>
+          <img src={image2} alt='' className='w-full h-full object-cover' />
+        </div>
+      </SwiperSlide>
+    </Swiper>
+  );
+};
+
+export default Slider;
