085607c80a0037e0ef361c7f10ab2efd5a2b3a35
Eric Niyonkuru <eric.niyonkuru@pesachoice.com>
2023-11-03 18:53:10 +0200

    add login and notice list UI

---
+++ src/pages/public/NoticePage.tsx
@@ -0,0 +1,11 @@
+import NoticeList from "../../components/notices/noticeList";
+
+export const NoticePage = () => {
+  return (
+    <>
+      <NoticeList />
+    </>
+  );
+};
+
+export default NoticePage;
