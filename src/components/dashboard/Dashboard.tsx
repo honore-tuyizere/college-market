555ab967203ca284ec4090f5b2f71bfcee015661
Jean Bosco MUGIRANEZA <mjeanbosco19@gmail.com>
2023-11-17 18:47:27 +0200

    ft(dashbaord): Overview and stastics

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,11 +1,12 @@
 import DashboardTopBar from "../layouts/navigation/TopBar";
+import DashHeader from "./DashHeader";
 
 export const Dashboard = () => {
   return (
     <>
-      <DashboardTopBar title='My page'>
-        <>Hello</>
+      <DashboardTopBar title='My Dashboard'>
       </DashboardTopBar>
+      <DashHeader />
     </>
   );
 };
